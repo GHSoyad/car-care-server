28d2a5d0384ae5c18e21f5638e8fe04f399a126a
GHSoyad <ghsoyad@gmail.com>
2022-11-12 14:07:30 +0600

    Added mongodb setup

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+// Middlewares
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7r6jn89.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const servicesCollection = client.db("car-care").collection("services");
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const service = await servicesCollection.findOne(query);
+            res.send(service);
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(error => (console.log(error)))
 
 
 app.get('/', (req, res) => {
